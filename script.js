3c9b8ccbadd99fe4d4102c8331f678a9b97f5c83
Veghing05 <renatosbv27@gmail.com>
2024-07-13 21:15:33 -0300

    feat: financiado

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-function calcularValorIdeal() {
-  // Obtendo os valores dos campos do formulário
+// Função para calcular valores do veículo alugado
+function calcularValorAlugado() {
   const aluguelMensal = parseFloat(document.getElementById('aluguelMensal').value);
   const quilometrosRodados = parseFloat(document.getElementById('quilometrosRodados').value);
   const precoCombustivel = parseFloat(document.getElementById('precoCombustivel').value);
   const consumoKmLitro = parseFloat(document.getElementById('consumoKmLitro').value);
   const lucroDesejado = parseFloat(document.getElementById('lucroDesejado').value);
 
-  // Calculando o custo do combustível por mês
   const custoCombustivel = (quilometrosRodados / consumoKmLitro) * precoCombustivel;
-
-  // Calculando o custo total mensal
   const custoTotalMensal = aluguelMensal + custoCombustivel;
-
-  // Calculando o custo por quilômetro
   const custoPorKm = custoTotalMensal / quilometrosRodados;
-
-  // Calculando a tarifa mínima para obter o lucro desejado
   const tarifaMinima = (custoTotalMensal + lucroDesejado) / quilometrosRodados;
-
-  // Calculando o lucro por quilômetro com a tarifa mínima
   const lucroPorKm = tarifaMinima - custoPorKm;
 
-  // Exibindo o resultado
+  const mensagemInformativa = document.getElementById('mensagemInformativa');
+  mensagemInformativa.innerHTML = `
+      <p>Lucro mensal desejado: R$ ${lucroDesejado.toFixed(2).replace('.', ',')}</p>
+      <p>Custo mensal: R$ ${custoTotalMensal.toFixed(2).replace('.', ',')}</p>
+      <p>Custo por quilômetro: R$ ${custoPorKm.toFixed(2).replace('.', ',')}</p>
+      <p>Com base nas informações fornecidas, para obter um lucro líquido de R$ ${lucroDesejado.toFixed(2).replace('.', ',')}, rodando ${quilometrosRodados.toFixed(2).replace('.', ',')} quilômetros no mês, você deve aceitar viagens com tarifas de, no mínimo, R$ ${tarifaMinima.toFixed(2).replace('.', ',')}.</p>
+      <p>Aceitando valores superiores a este, seu lucro por quilômetro será de R$ ${lucroPorKm.toFixed(2).replace('.', ',')}.</p>
+  `;
+}
+
+// Função para calcular valores do veículo financiado
+function calcularValorFinanciado() {
+  const valorAutomovel = parseFloat(document.getElementById('valorAutomovel').value);
+  const valorParcela = parseFloat(document.getElementById('valorParcela').value);
+  const ipva = parseFloat(document.getElementById('ipva').value);
+  const depreciacaoAnual = parseFloat(document.getElementById('depreciacaoAnual').value) / 100;
+  const quilometrosRodados = parseFloat(document.getElementById('quilometrosRodados').value);
+  const seguroMensal = parseFloat(document.getElementById('seguroMensal').value);
+  const custoPneus = parseFloat(document.getElementById('custoPneus').value);
+  const vidaUtilPneus = parseFloat(document.getElementById('vidaUtilPneus').value);
+  const custoTrocaOleo = parseFloat(document.getElementById('custoTrocaOleo').value);
+  const frequenciaTrocaOleo = parseFloat(document.getElementById('frequenciaTrocaOleo').value);
+  const precoCombustivel = parseFloat(document.getElementById('precoCombustivel').value);
+  const consumoKmLitro = parseFloat(document.getElementById('consumoKmLitro').value);
+  const lucroDesejado = parseFloat(document.getElementById('lucroDesejado').value);
+
+  const depreciacaoMensal = (valorAutomovel * depreciacaoAnual) / 12;
+  const custoMensalPneus = (custoPneus / vidaUtilPneus) * quilometrosRodados;
+  const custoMensalTrocaOleo = (custoTrocaOleo / frequenciaTrocaOleo) * quilometrosRodados;
+  const custoCombustivel = (quilometrosRodados / consumoKmLitro) * precoCombustivel;
+  const custoTotalMensal = valorParcela + ipva + depreciacaoMensal + seguroMensal + custoMensalPneus + custoMensalTrocaOleo + custoCombustivel;
+  const custoPorKm = custoTotalMensal / quilometrosRodados;
+  const tarifaMinima = (custoTotalMensal + lucroDesejado) / quilometrosRodados;
+  const lucroPorKm = tarifaMinima - custoPorKm;
+
   const mensagemInformativa = document.getElementById('mensagemInformativa');
   mensagemInformativa.innerHTML = `
       <p>Lucro mensal desejado: R$ ${lucroDesejado.toFixed(2).replace('.', ',')}</p>
